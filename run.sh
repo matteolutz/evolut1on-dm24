a76f63d7793fb596c5b7800143eada4a617f8177
matteolutz <matteo.lutz06@outlook.de>
2024-04-29 17:41:55 +0200

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Update
+sudo apt-get update
+
+# Install Node.js
+sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+
+sudo apt install nodejs build-essential -y
+
 # Install dependencies
 npm install
 
